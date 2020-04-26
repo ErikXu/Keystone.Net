29db2ff200b9f73491cb2bf088f2e848feb203a7
xuzhe <xuzhe@q1.com>
2020-04-26 20:37:24 +0800

    Add domain service.

--- src/Keystone.Net/Services/DomainService.cs
+++ src/Keystone.Net/Services/DomainService.cs
@@ -1,15 +1,22 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Keystone.Net.Services
 {
+    /// <summary>
+    /// https://docs.openstack.org/api-ref/identity/v3/index.html?expanded=#domains
+    /// </summary>
     public class DomainService : AbstractService
     {
         public DomainService(HttpClient client) : base(client)
         {
         }
 
+        /// <summary>
+        /// List domains
+        /// </summary>
         public async Task<Response<JObject>> List(string token)
         {
             var request = new Request
@@ -21,5 +28,91 @@ namespace Keystone.Net.Services
 
             return await ExecuteAsync<JObject>(request);
         }
+
+        /// <summary>
+        /// Create domain
+        /// </summary>
+        public async Task<Response<JObject>> Create(string token, Domain domain)
+        {
+            var form = new { domain };
+            var body = Serialize(form);
+
+            var request = new Request
+            {
+                Uri = "/v3/domains",
+                Method = HttpMethod.Post,
+                Token = token,
+                Body = body
+            };
+
+            return await ExecuteAsync<JObject>(request);
+        }
+
+        /// <summary>
+        ///Show domain details
+        /// </summary>
+        public async Task<Response<JObject>> Details(string token, string id)
+        {
+            var request = new Request
+            {
+                Uri = $"/v3/domains/{id}",
+                Method = HttpMethod.Get,
+                Token = token,
+            };
+
+            return await ExecuteAsync<JObject>(request);
+        }
+
+        /// <summary>
+        /// Update domain
+        /// </summary>
+        public async Task<Response<JObject>> Update(string token, string id, Domain domain)
+        {
+            var form = new { domain };
+            var body = Serialize(form);
+
+            var request = new Request
+            {
+                Uri = $"/v3/domains/{id}",
+                Method = new HttpMethod("PATCH"),
+                Token = token,
+                Body = body
+            };
+
+            return await ExecuteAsync<JObject>(request);
+        }
+
+        /// <summary>
+        /// Delete  domain
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Response<JObject>> Delete(string token, string id)
+        {
+            var request = new Request
+            {
+                Uri = $"/v3/domains/{id}",
+                Method = HttpMethod.Delete,
+                Token = token,
+            };
+
+            return await ExecuteAsync<JObject>(request);
+        }
+    }
+
+    public class Domain
+    {
+        [JsonProperty("enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("options")]
+        public object Options { get; set; }
     }
 }
